179a436a09449fbf5c42f068ce31e39ece665e87
Albertus Chua <albertus.chua@gmail.com>
2022-10-19 15:59:06 -0400

    Standup coding challenge for 10/19/22

--- standup/standup.js
+++ standup/standup.js
@@ -56,3 +56,24 @@ function domainName(url){
 function sumMix(arr) {
     return arr.reduce((total, num) => total += Number(num), 0);
 }
+
+// 10/19/22
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+// getAverage([2,2,2,2]),2)
+// getAverage([1,2,3,4,5,]),3);
+// getAverage([1,1,1,1,1,1,1,2]),1)
+
+function getAverage(arr) {
+    // Find the sum of all the elements
+    // Divide that sum by the length of the array
+    // Round it down to the nearest integer
+
+    const average = arr.reduce((total, num) => total += num, 0) / arr.length;
+
+    return Math.floor(average);
+}
