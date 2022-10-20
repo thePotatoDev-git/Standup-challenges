f3527fa989406299e80ea6111623b78276126db0
Albertus Chua <albertus.chua@gmail.com>
2022-10-20 15:42:00 -0400

    Standup challenge for 10/20/22

--- standup/standup.js
+++ standup/standup.js
@@ -77,3 +77,30 @@ function getAverage(arr) {
 
     return Math.floor(average);
 }
+
+// 10/20/22
+// You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+// repeats([4,5,7,5,4,8]),15)
+// repeats([9, 10, 19, 13, 19, 13]),19)
+// repeats([16, 0, 11, 4, 8, 16, 0, 11]),12)
+// repeats([5, 17, 18, 11, 13, 18, 11, 13]),22)
+// repeats([5, 10, 19, 13, 10, 13]),24)
+
+function repeats (arr) {
+    // Make an empty array
+    // Loop through the array, and if the number is repeated, push it into the empty array
+    // Filter arr to not include the repeated numbers
+
+    const repeated = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) {
+            repeated.push(arr[i]);
+        }
+    }
+    
+    return arr.filter(num => !repeated.includes(num)).reduce((total, num) => total += num, 0);
+}
