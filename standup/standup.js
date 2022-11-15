ec7f3a2be3cbed3fdc265397088d9a6e55be0844
Albertus Chua <albertus.chua@gmail.com>
2022-11-15 17:21:06 -0500

    Standup challenge for 11/15/22

--- standup/standup.js
+++ standup/standup.js
@@ -359,3 +359,36 @@ function middleNum(arr) {
 console.log(middleNum([3, 1, 2]), 2);
 console.log(middleNum([4, 1, 20]), 0);
 console.log(middleNum([20, 15, 4]), 1);
+
+// 11/15/22
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+// sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
+
+function sortArray(arr) {
+    // Make an array with only the odd numbers
+    // Sort that array in ascending order
+    // Loop through the original array. If it's odd, replace that number with the first element in the oddNums array. Then remove the number from that oddNums array.
+
+    const oddNumsSorted = arr.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+
+    // return arr.map(num => {
+    //     if (num % 2 !== 0) {
+    //        return oddNumsSorted.shift();
+    //     } else {
+    //         return num;
+    //     }
+    // });
+
+    return arr.map(num => num % 2 !== 0 ? oddNumsSorted.shift() : num);
+}
+
+console.log(sortArray([7, 1]), [1, 7]);
+console.log(sortArray([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4]);
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]);
