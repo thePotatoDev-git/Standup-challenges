fb36a00a93df3981eb7100a65e0e3ef2a9809ed3
Albertus Chua <albertus.chua@gmail.com>
2022-12-06 08:56:04 -0800

    Standup challenge for 12/6/22

--- standup/standup.js
+++ standup/standup.js
@@ -577,3 +577,49 @@ function charCount(str) {
 console.log(charCount('aba'), "{'a': 2, 'b': 1}");
 console.log(charCount('hello'), "{'e': 1, 'h': 1, 'l': 2, 'o': 1}");
 console.log(charCount('abba'), "{'a': 2, 'b': 2}");
+
+// 12/6/22
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+const numJewelsInStones = function(jewels, stones) {
+    // Split jewels and stones into individual elements
+    // Find the occurrences in stones and store them in an object
+    // Find the jewels in the stoneCount object and add them together
+    
+    jewels = jewels.split('');
+    stones = stones.split('');
+    const stoneCount = {};
+    let totalJewels = 0;
+
+    
+    for (const stone of stones) {
+        stoneCount[stone] = stoneCount[stone] ? stoneCount[stone] + 1 : 1;
+    }
+
+    jewels.forEach(jewel => {
+        stoneCount[jewel] ? totalJewels += stoneCount[jewel] : totalJewels;
+    });
+
+    return totalJewels;
+};
+
+console.log(numJewelsInStones('aA', 'aAAbbbb'), '3');
+console.log(numJewelsInStones('z', 'ZZ'), '0');
