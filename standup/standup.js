367e65f98bbdb1e9c7b3e002774d17d6f1f1867f
Albertus Chua <albertus.chua@gmail.com>
2022-12-13 16:07:47 -0500

    Standup challenge for 12/13/22

--- standup/standup.js
+++ standup/standup.js
@@ -713,3 +713,46 @@ function containsDuplicate(arr) {
 console.log(containsDuplicate([1, 2, 3, 4]), 'false');
 console.log(containsDuplicate([1, 1, 2, 3, 3, 4, 5]), 'true');
 console.log(containsDuplicate([1, 1, 1, 1, 1]), 'true');
+
+// 12/13/22
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+function majorityElement(arr) {
+    // Make a hashCount variable with an empty object
+    // Make a majority variable with 0 value
+    // Make an element variable that is empty
+    // Count the occurrences for each element
+    // Loop through the object. If the value is bigger than 'majority', replace it with that number, and change 'element' to that number
+
+    const hashCount = {};
+    let majority = 0;
+    let element = '';
+
+    for (num of arr) {
+        hashCount[num] = hashCount[num] ? hashCount[num] + 1 : 1;
+    }
+
+    for (const el in hashCount) {
+        if (hashCount[el] > majority) {
+            majority = hashCount[el];
+            element = el;
+        }
+    }
+
+    return element
+}
+
+console.log(majorityElement([3, 2, 3]), '3');
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2,]), '2');
+console.log(majorityElement([3, 2, 1, 2, 1, 3, 3]), '3');
